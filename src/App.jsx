d721635b544201376cf33354005600337c1277aa
shadyaahh <tshadiya4@gmail.com>
2025-10-31 11:02:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,47 @@
 // import Calling_function from './embedding-expression/Calling_function'
 // import Welcome from './embedding-expression/Welcome'
 
+// import Greeting from "./functional-components/Greeting"
+// import Props from "./functional-components/Props"
 
-// function App() {
+// import Props from "./functional-components/Props"
 
-//   return (
-//     <>
+// import State from "./functional-components/State"
+
+// import Basic from "./functional-components/Basic"
+
+
+function App() {
+
+  return (
+    <>
     {/* ---------------------------Embedding Expression---------------- */}
       {/* <Welcome /> */}
       {/* <Calling_function/> */}
       {/* <Conditional/> */}
       {/* <RenderingLists/> */}
 
-//      </>
-//   )
-// }
+      {/*---------------functional componets---------------------*/}
+    {/* <Basic/> */}
+    {/* <State/> */}
+    {/* <Props/> */}
+    
+      
 
-// export default App
+     </>
+  )
+}
+
+export default App
 
 
 
 
 // {/* -------------------class Component------------- */}
 
-import React, { Component } from 'react'
-import UnMounting from '../class-components/live-cycle-methods/UnMounting'
+// import React, { Component } from 'react'
+// import UnMounting from '../class-components/live-cycle-methods/UnMounting'
+// import Timer from '../class-components/life-cycle-methods/Time'
 // import Updating from '../class-components/live-cycle-methods/Updating'
 // import Mounting from '../class-components/live-cycle-methods/mounting'
 // import Counter2 from '../class-components/counter2'
@@ -36,19 +53,21 @@ import UnMounting from '../class-components/live-cycle-methods/UnMounting'
 // import Counter from '../class-components/Counter'
 // import Basic from '../class-components/Basic'
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <>
       {/* <Basic/> */}
       {/* <UsingProps name="shadiya"/> */}
       {/* <Counter/> */}
       {/* <Counter2/> */}
       {/* <Mounting/> */}
       {/* <Updating/> */}
-      <UnMounting/>
-      </>
+      {/* <UnMounting/> */}
+      {/* <Time/> */}
+      {/* </>
     )
   }
-}
+} */}
+
 
---
+++ src/functional-components/Basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic = () => {
+    let name = "shadyyah";
+  return (
+    <div>
+      <h1>Hello, {name}</h1>
+    </div>
+  )
+}
+
+export default Basic
